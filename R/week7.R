b3d941346531002af5514b0bde11b19539a07255
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-03-13 12:29:36 -0500

    class solution

--- R/week7.R
+++ R/week7.R
@@ -1,60 +1,63 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+update.packages(ask=FALSE)
 library(tidyverse)
 library(GGally)
-
+library(conflicted)
 
 
 # Data Import and Cleaning
 week7_tbl <- read.csv(file = "../data/week3.csv") |>
-  rename(Gender=gender, Condition=condition) |>
-  mutate(across(starts_with("time"), as.POSIXct)) |>
-  mutate(Gender = factor(Gender, levels=c("M","F"),
-                         labels = c("Male","Female"))) |>
-  mutate(Condition=factor(Condition, levels=c("A", "B", "C"),
-                           labels=c("Block A", "Block B", "Control"))) |> 
-  filter(q6 == 1) |>
-  select(-q6) |>
-  mutate(timeSpent= as.integer(difftime(timeEnd, timeStart)))
+  mutate(timeStart = ymd_hms(timeStart),
+         timeEnd = ymd_hms(timeStart),
+         gender = factor(gender, levels=c("M","F"),
+                         labels = c("Male","Female")),
+         condition=factor(condition, levels=c("A", "B", "C"),
+                           labels=c("Block A", "Block B", "Control")),
+         timeSpent=as.integer(timeEnd - timeStart)) |> 
+  dplyr::filter(q6 == 1) |>
+  select(-q6) 
 
 
 # Visualization
-(ggpairs(week7_tbl, 
-        columns = 5:13)) |> 
-  ggsave(filename="../figs/fig0.png")
+week7_tbl |>
+  select(q1:q10) |> 
+  ggpairs()
+
 (ggplot(week7_tbl, aes(timeStart, q1))+
     geom_point() +
     xlab("Date of Experiment") +
     ylab("Q1 Score") ) |> 
-  ggsave(filename="../figs/fig1.png", width = 5, height = 3)
+    ggsave(filename="../figs/fig1.png", units = "px", width = 1920, height = 1080)
 
-(ggplot(week7_tbl, aes(q1,q2, color=Gender))+
-  geom_point() +
+(ggplot(week7_tbl, 
+        aes(q1,q2, color=gender))+
   geom_jitter() +
   scale_color_discrete(name="Participant Gender")) |> 
-  ggsave(filename="../figs/fig2.png",width = 5, height = 3)    
-(ggplot(week7_tbl, aes(q1,q2))+
-  geom_point() +
+  ggsave(filename="../figs/fig2.png", units = "px", width = 1920, height = 1080)    
+(ggplot(week7_tbl, 
+        aes(q1,q2))+
   geom_jitter() +
-  facet_grid(cols = vars(Gender))+
-  labs(x="Score on Q1", y="Score on Q2") )|> 
-  ggsave(filename="../figs/fig3.png", width = 5, height = 3)  
+  xlab("Score on Q1") +
+  ylab("Score on Q2") +
+  facet_grid(cols = vars(gender))) |> 
+  ggsave(filename="../figs/fig3.png", units = "px", width = 1920, height = 1080)  
 
-(ggplot(week7_tbl, aes(Gender,timeSpent))+
+(ggplot(week7_tbl, aes(gender,timeSpent))+
   geom_boxplot()+
   labs(y="Time Elapsed (mins)")) |> 
-  ggsave(filename="../figs/fig4.png",width = 5, height = 3)
+  ggsave(filename="../figs/fig4.png", units = "px", width = 1920, height = 1080)
 
 
-(ggplot(week7_tbl, aes(q5, q7, color=Condition)) +
-  geom_point()+
-  geom_jitter()+
+(ggplot(week7_tbl, 
+        aes(q5, q7, color=condition)) +
+  geom_jitter(width=.1)+
   geom_smooth(method = "lm", se = FALSE)+
   scale_color_discrete(name="Experimental Condition") +
   labs(x="Score on Q5", y="Score on Q7") +
   theme(legend.position = "bottom",
         legend.background = element_rect(fill = "#E0E0E0"))) |> 
-  ggsave(filename="../figs/fig5.png",width = 5, height = 3)
+  ggsave(filename="../figs/fig5.png", units = "px", width = 1920, height = 1080)
 
 
 
