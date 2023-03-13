1dc4dcd6cf01ab58ee151ea0b5cda58dbf5ab7f7
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-03-13 00:31:43 -0500

    Line 25-30: Check for differences in Q1 over time by creating a plot of timeStart on the x-axis and q1 on the y-axis that looks like Fig 1. Line 31-35: Check for gender differences in the Q1/Q2 relationship by creating a figure that looks like Fig 2.

--- R/week7.R
+++ R/week7.R
@@ -13,3 +13,38 @@ week7_tbl <- read.csv(file = "../data/week3.csv") |>
   filter(q6 == 1) |>
   select(-q6) |>
   mutate(timeSpent= difftime(timeEnd, timeStart))
+
+
+
+
+
+# Visualization
+fig0 <- week7_tbl 
+
+
+(ggplot(week7_tbl, aes(timeStart, q1))+
+    geom_point() +
+    xlab("Date of Experiment") +
+    ylab("Q1 Score") ) |> 
+  ggsave(filename="../figs/fig1.png", width = 5, height = 3)
+
+(ggplot(week7_tbl, aes(q1,q2, color=Gender))+
+  geom_point() +
+  geom_jitter() +
+  scale_color_discrete(name="Participant Gender", labels=c("Female", "Male"))) |> 
+  ggsave(filename="../figs/fig2.png",width = 5, height = 3)    
+ggplot(week7_tbl, aes(q1,q2))+
+  geom_point() +
+  geom_jitter() +
+  labs(color = "Participant Gender") 
+
+
+#ggsave(filename="../figs/fig3.png")  
+ggplot(week7_tbl, aes(q1,q2))+
+  geom_point() +
+  geom_jitter() +
+  labs(color = "Participant Gender") 
+
+#ggsave(filename="../figs/fig4.png")
+
+
